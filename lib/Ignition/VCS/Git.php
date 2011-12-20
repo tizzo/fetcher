b0504079fbab3225278d351db11de7521aa67840
Howard Tyson <howard@howardtyson.com>
2011-12-20 07:48:27 -0800

    Begninning to flesh out git provider

--- lib/Ignition/VCS/Git.php
+++ lib/Ignition/VCS/Git.php
@@ -4,7 +4,31 @@ namespace Ignition\VCS;
 
 class Git {
 
+  public function checkout() {
+  }
+
   public function update($localDirectory) {
     //update = "cd %s; git checkout %s; git pull; git submodule update --init" % (localDirectory, label)
+  }
+
+  private function executeGitCommand($command) {
+    // TODO: Allow the git path to be specified?
+    $command = 'git ' . $command;
+    if (drush_context('DRUSH_VERBOSE')) {
+      $function = 'drush_shell_exec_interactive';
+    }
+    else {
+      $function = 'drush_shell_exec';
+    }
+    $status = $function($command);
+    if ($status == 128) {
+      return FALSE;
+    }
+    else if ($status) {
+      return FALSE;
+    }
+    else {
+      return TRUE;
+    }
   }
 }
