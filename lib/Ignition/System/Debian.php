8c2b54155bbe71cb067f1bc50a811d7c308f0873
Howard Tyson <howard@howardtyson.com>
2011-12-20 07:48:13 -0800

    Creating POSIX provider that all posix systems will inherit from.

--- lib/Ignition/System/Debian.php
+++ lib/Ignition/System/Debian.php
@@ -3,7 +3,7 @@
 namespace Ignition\System;
 
 // 
-class Debian /*extends Posix implements Ignition\System\SystemInterface*/ {
+class Debian extends Posix {
 
   public function __construct() {
   }
@@ -22,43 +22,5 @@ class Debian /*extends Posix implements Ignition\System\SystemInterface*/ {
 
   public function buildApacheRestart() {
     return '/etc/init.d/apache2 restart';
-  }
-
-  /**
-   * 
-   * TODO: Move to Posix provider.
-   */
-  public function ensureFolderExists($path, $owning_user = NULL, $owning_group = NULL) {
-    $old_mask = umask(0);
-    $path_parts = explode('/', $path);
-    $success = TRUE;
-    $path = '';
-    foreach ($path_parts as $part) {
-      $path .= $part . '/';
-      if ($success && !is_dir($path)) {
-        drush_log("Creating folder $path");
-        $success = mkdir($path, 0755);
-      }
-    }
-    umask($old_mask);
-    return $success;
-  }
-
-  /**
-   * Ensure that a file exists and is owned by the appropriate user.
-   *
-   * TODO: Move to Posix provider.
-   */
-  public function ensureFileExists($path, $owning_user = NULL, $owning_group = NULL) {
-    $old_mask = umask(0);
-    $path_parts = explode('/', $path);
-    $filename = array_pop($path_parts);
-    $directory = implode('/', $path_parts);
-    $this->ensureFolderExists($directory, $owning_user, $owning_group);
-    if (!file_exists($path)) {
-      drush_log("Creating file $path");
-      touch($path);
-      chmod($path, 755);
-    }
   }
 }
