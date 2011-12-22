f757b581abd281d335585dcc70410246767cc809
Howard Tyson <howard@howardtyson.com>
2011-12-22 09:46:41 -0800

    Updated MySQL handler and added a call to ensure the database exists to the get command.

--- lib/Ignition/DB/Mysql.php
+++ lib/Ignition/DB/Mysql.php
@@ -11,26 +11,53 @@ class Mysql extends Base {
 
   private $database = FALSE;
 
-  /**
-   *
-   */
-  public function configure($configure) {
+  private $db_spec = array();
+
+  public function configure($config) {
     foreach ($config as $name => $value) {
-      if (isset($this->{$name})) {
+      if (isset($this->{$name}) && $value != '') {
         $this->{$name} = $value;
       }
     }
   }
 
-  public function exists() {
-    $sql = 'SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = "%s"';
-  }
-  
-  public function createDatabase($database) {
-    // TODO: escape database name.
-    shell_exec(sprintf("mysql -e 'create database %s'", $database));
+  /**
+   * Returns the drush driver for this database.
+   */
+  public function getDriver() {
+    return 'mysql';
   }
 
+  public function exists() {
+    $db_spec = $this->getAdminDbSpec();
+    return drush_sql_db_exists($db_spec);
+  }
+
+  public function getAdminDbSpec() {
+    $adminDBSpec = $this->db_spec;
+    // If we have an option set for the sql user, use it.
+    $adminDBSpec['username'] = drush_get_option('ignition-sql-user', 'root');
+    // If we have an option set for the sql password, use it.
+    if (drush_get_option('ignition-sql-password', FALSE)) {
+      $adminDBSpec['password'] = drush_get_option('ignition-sql-password');
+    }
+    else {
+      unset($adminDBSpec['password']);
+    }
+    return $adminDBSpec;
+  }
+  
+  public function createDatabase() {
+    // TODO: escape database name.
+    //shell_exec(sprintf("'create database %s'", $database));
+    $adminDBSpec = $this->getAdminDbSpec();
+    return drush_sql_empty_db($adminDBSpec);
+  }
+
+  public function checkUserExists($user) {
+    $sql = "SELECT user FROM mysql.user WHERE user='%s'";
+    //shell_exec(sprintf("mysql -e 'create database %s'", $database));
+  }
   public function createUser($database, $password) {
     //sql = "mysql -e 'create user \"%s\"@\"localhost\" identified by \"%s\"'" % (siteName, password)
   }
@@ -38,6 +65,19 @@ class Mysql extends Base {
   public function grantAccessToUser($database) {
     //sql = "mysql -e 'grant all on %s.* to \"%s\"@\"localhost\"'" % (siteName, siteName)
     //sql = "mysql -e 'flush privileges'"
+  }
+
+  protected function executeQuery($command) {
+    // TODO: Allow the mysql path to be specified?
+    $command = 'mysql -e "' . $command . ';"';
+
+    if (drush_get_context('DRUSH_VERBOSE')) {
+      $function = 'drush_shell_exec_interactive';
+    }
+    else {
+      $function = 'drush_shell_exec';
+    }
+    return call_user_func_array($function, $args);
   }
 }
 
