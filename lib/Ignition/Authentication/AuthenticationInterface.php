dea905d29af1f6fc8d056c00dab1b6c71f22ed7a
Howard Tyson <howard@howardtyson.com>
2012-01-04 10:40:25 -0800

    Got our public key authentication plugin working.

--- lib/Ignition/Authentication/AuthenticationInterface.php
+++ lib/Ignition/Authentication/AuthenticationInterface.php
@@ -7,8 +7,12 @@ namespace Ignition\Authentication;
 interface AuthenticationInterface {
 
   /**
+   * Recieve a client object similar to \Ignition\Utility\HTTPClient() and add authentication parameters.
    *
+   * @param $client
+   *   An HTTPClient descended object.
+   * @return Void
    */
-  static public function addAuthenticationToHTTPClientFromDrushContext($client, $container);
+  public function addAuthenticationToHTTPClientFromDrushContext(\Ignition\Utility\HTTPClient $client);
 
 }
