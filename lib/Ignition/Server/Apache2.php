bb701062ee6a797d7d47df650ca5fb15ed18fc7e
Howard Tyson <howard@howardtyson.com>
2012-01-06 21:50:27 -0800

    Adding a method to the Server class to get the web user.

--- lib/Ignition/Server/Apache2.php
+++ lib/Ignition/Server/Apache2.php
@@ -3,5 +3,14 @@
 namespace Ignition\Server;
 
 class Apache2 {
+
+  /**
+   * Get the web user for this server.
+   */
+  public function webUser() {
+    // TODO: Allow some kind of loading from the system.
+    return 'www-data';
+  }
+
 }
 
