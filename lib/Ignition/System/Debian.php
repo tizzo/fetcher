72e289299d950e3f6b4da471181c0ea0200b9555
Howard Tyson <howard@howardtyson.com>
2012-01-09 16:19:16 -0800

    Deleting deprecated methods in the Debian class that have been moved to the server provider.

--- lib/Ignition/System/Debian.php
+++ lib/Ignition/System/Debian.php
@@ -5,19 +5,4 @@ namespace Ignition\System;
 // 
 class Debian extends Posix {
 
-  /** 
-   * 
-   */
-  public function getWebRoot() {
-    return '/var/www';
-  }
-
-  // TODO: This really should be different for different servers.
-  public function getWebUser() {
-    return 'www-data';
-  }
-
-  public function buildApacheRestart() {
-    return '/etc/init.d/apache2 restart';
-  }
 }
