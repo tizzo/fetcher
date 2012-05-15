19468d0b88d762334e307c27a84b8164a1fe2f93
Howard Tyson <howard@howardtyson.com>
2012-05-15 12:46:00 -0400

    Improving comments.

--- assets/drush.alias.tpl.php
+++ assets/drush.alias.tpl.php
@@ -5,7 +5,7 @@ $aliases['local'] = array(
 );
 
 $aliases['live'] = array(
-<?php /* TODO: Iterate over environments:ta. */ ?>
+<?php /* TODO: Iterate over environments. */ ?>
   'root' => '<?php print trim($remote_root); ?>',
   'remote-host' => '<?php print trim($hostname); ?>',
 <?php /* TODO: Add this to the service and make it dynamic. */ ?>
