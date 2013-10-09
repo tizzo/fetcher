66275b8060a9967eca26a61c892653f639e35159
Howard Tyson <howard@howardtyson.com>
2013-10-08 19:49:37 -0700

    Moving task loader tests to their own class.

--- tests/taskTest.php
+++ tests/taskTest.php
@@ -13,37 +13,9 @@ use \Fetcher\Tests\Fixtures\Tasks\TaskAnnotation,
 class TaskTest extends PHPUnit_Framework_TestCase {
 
   /**
-   * Test the scanObject() method.
+   * 
    */
-  public function testScanObject() {
-    $loader = new TaskLoader();
-    $annotatedObject = new TaskAnnotation();
-    $tasks = $loader->scanObject($annotatedObject);
-    $this->assertNotEmpty($tasks);
-    $task = array_pop($tasks);
-    $this->assertEquals($task['description'], 'Provides a sample task for parsing.');
-    $this->assertEquals($task['fetcher_task'], 'some_task_name');
-    $this->assertEquals($task['before_message'], 'We are about to run a task.');
-    $this->assertEquals($task['after_message'], 'We have just run a task.');
-    $this->assertContains('foo', $task['before_task']);
-    $this->assertContains('bar', $task['before_task']);
-    $this->assertContains('baz', $task['after_task']);
-  }
-
-  /**
-   * Test scanning a class with a bad definition
-   */
-  public function testScanBadObjectThrowsExceptions() {
-    $loader = new TaskLoader();
-    $annotatedObject = new TaskAnnotationError();
-    try {
-      $loader->scanObject($annotatedObject);
-      // If we don't throw an exception in the line above something went wrong.
-      throw new \Exception('Scanning the TaskAnnotationError class should have throw a TaskLoaderException.');
-    }
-    catch (TaskLoaderException $exception) {
-      $this->assertInstanceOf('\Fetcher\Task\TaskLoaderException', $exception);
-    }
+  public function testPrepMessage() {
   }
 
 }
