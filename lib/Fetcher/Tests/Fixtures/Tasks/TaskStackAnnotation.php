0d40ed9e52451b0d27ee8327a56182d51916003f
Howard Tyson <howard@howardtyson.com>
2014-02-10 14:08:49 -0500

    Adding accidentally omitted task annotation fixture.

---
+++ lib/Fetcher/Tests/Fixtures/Tasks/TaskStackAnnotation.php
@@ -0,0 +1,30 @@
+<?php
+namespace Fetcher\Tests\Fixtures\Tasks;
+
+class TaskStackAnnotation {
+
+  /**
+   * A function that is not a task.
+   */
+  public function notATask() {
+    return 'This is not a task.';
+  }
+
+  /**
+   * @fetcherTask first_stack_1 
+   * @description Provides a sample task for parsing.
+   * @stack test_stack_1
+   */
+  public function simpleMethodOne($site) {
+    $site['log']('The first method from test_stack_1 has run.');
+  }
+
+  /**
+   * @fetcherTask first_stack_2 
+   * @description Provides a anther sample task for parsing.
+   * @stack test_stack_1
+   */
+  public function simpleMethodTwo($site) {
+    $site['log']('The second method from test_stack_1 has run.');
+  }
+}
