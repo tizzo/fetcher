670f50ad37c80eeba494a137546ac2ef5ed1c717
Howard Tyson <howard@howardtyson.com>
2014-02-19 11:45:58 -0500

    Moving more Task boilerplate into AbstractTask and adding a method to specify a dependency.

--- lib/Fetcher/Task/AbstractTask.php
+++ lib/Fetcher/Task/AbstractTask.php
@@ -4,6 +4,28 @@ namespace Fetcher\Task;
 
 abstract class AbstractTask implements TaskInterface {
 
+  // The machine name of this task.
+  public $fetcherTask = NULL;
+
+  // The description for this task for the task list.
+  public $description = NULL;
+
+  // A log message to print before running the task.
+  public $beforeMessage = NULL;
+
+  // A log message to print after the callable has been run successfully.
+  public $afterMessage = NULL;
+
+  // A log message to print after the callable has been run successfully.
+  public $callable = NULL;
+
+  // Any defined task stacks to add this task to.
+  public $stacks = array();
+
+  // Any configuration provided to this task.
+  public $arguments = array();
+
+
   /**
    * Constructor.
    *
@@ -13,8 +35,6 @@ abstract class AbstractTask implements TaskInterface {
   public function __construct($fetcherTask = NULL) {
     $this->fetcherTask = $fetcherTask;
   }
-
-
 
   /**
    * Run this task.
@@ -29,7 +49,53 @@ abstract class AbstractTask implements TaskInterface {
     }
   }
 
-
+  /**
+   * Run the internal logic for this task.
+   */
   abstract function performAction($site, $arguments);
+
+  /**
+   * Prepare log messages by substituting 
+   *
+   * Log messages can use data from any config key if the log message uses the format
+   * [[array_key]].
+   *
+   * @param $string
+   *   The string on which to perform substitutions.
+   * @param $values
+   *   An array or oject implementing array access.
+   *   Usually a \Fetcher\Site object.
+   */
+  public function prepMessage($string, $values) {
+    $callback = function($matches) use ($values) {
+      if (isset($values[$matches[1]])) {
+        return $values[$matches[1]];
+      }
+    };
+    $pattern = '/\[\[(.*)\]\]/U';
+    return \preg_replace_callback($pattern, $callback, $string);
+  }
+
+  /**
+   * Add a stack dependency.
+   *
+   * @param $stackName
+   *    The name of the task stack this task should be a member of.
+   * @param $taskName
+   *    The name of the task nam of the task this task should relate to.
+   * @param $position
+   *    Whether to go `before` or `after` the specified task.
+   */
+  public function addTaskStackDependency($stackName, $taskName = NULL, $position = 'after') {
+    if (!in_array($position, array('before', 'after'))) {
+      throw new TaskLoaderException('Invalid position specificied.');
+    }
+    if (!isset($this->stacks[$stackName])) {
+      $this->stacks[$stackName] = array();
+    }
+    if (!is_null($taskName)) {
+      $this->stacks[$stackName][$position . 'Task'][] = $taskName;
+    }
+  }
 
 }
--- lib/Fetcher/Task/Task.php
+++ lib/Fetcher/Task/Task.php
@@ -5,27 +5,6 @@ use Symfony\Process;
 
 class Task extends AbstractTask implements TaskInterface {
 
-  // The machine name of this task.
-  public $fetcherTask = NULL;
-
-  // The description for this task for the task list.
-  public $description = NULL;
-
-  // A log message to print before running the task.
-  public $beforeMessage = NULL;
-
-  // A log message to print after the callable has been run successfully.
-  public $afterMessage = NULL;
-
-  // A log message to print after the callable has been run successfully.
-  public $callable = NULL;
-
-  // Any defined task stacks to add this task to.
-  public $stacks = array();
-
-  // Any configuration provided to this task.
-  public $arguments = array();
-
   /**
    * Run the internal logic for this task.
    */
@@ -36,25 +15,5 @@ class Task extends AbstractTask implements TaskInterface {
     \call_user_func_array($this->callable, array($site) + $arguments);
   }
 
-  /**
-   * Prepare log messages by substituting 
-   *
-   * Log messages can use data from any config key if the log message uses the format
-   * [[array_key]].
-   *
-   * @param $string
-   *   The string on which to perform substitutions.
-   * @param $values
-   *   An array or oject implementing array access.
-   *   Usually a \Fetcher\Site object.
-   */
-  public function prepMessage($string, $values) {
-    $callback = function($matches) use ($values) {
-      if (isset($values[$matches[1]])) {
-        return $values[$matches[1]];
-      }
-    };
-    $pattern = '/\[\[(.*)\]\]/U';
-    return \preg_replace_callback($pattern, $callback, $string);
-  }
+
 }
