03afe726482064e20d4a2b89c41df8258eec67c1
Jody Hamilton <jody@zivtech.com>
2016-05-17 07:53:27 -0700

    Allow use of specific Drupal versions e.g. 8.1.1

--- lib/Fetcher/Configurator/DrupalVersion.php
+++ lib/Fetcher/Configurator/DrupalVersion.php
@@ -7,7 +7,8 @@ use Fetcher\SiteInterface;
 class DrupalVersion implements ConfiguratorInterface {
 
   static public function configure(SiteInterface $site) {
-    $class = '\Fetcher\Configurator\DrupalVersion\Drupal' . $site['version'];
+    // Here version can be something like 8.1 or 7.x-dev but fetcher needs to know the major version.
+    $class = '\Fetcher\Configurator\DrupalVersion\Drupal' . substr($site['version'], 0, 1);
     if (class_exists($class)) {
       $class::configure($site);
     }
