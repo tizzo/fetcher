7713eecbdeaf4bd09674674e191763d256c4c692
Laurence Liss <laurence@zivtech.com>
2016-08-24 15:34:03 -0400

    Adjust vhosts to use Drupal's .htaccess file.

--- assets/drupal.6.vhost.tpl.php
+++ assets/drupal.6.vhost.tpl.php
@@ -1,46 +1,24 @@
 <Directory <?php print $docroot; ?> >
-  Options FollowSymLinks
-  AllowOverride None
-  # Protect files and directories from prying eyes.
-  <FilesMatch "\.(engine|inc|info|install|module|profile|po|schema|sh|.*sql|theme|tpl(\.php)?|xtmpl)$|^(code-style\.pl|Entries.*|Repository|Root|Tag|Template)$">
-    Order allow,deny
+  Options -Indexes
+  Options +FollowSymLinks
+  AllowOverride All
+
+  # Not a part of Drupal's stock .htaccess but added as a measure of security.
+  <FilesMatch "(^LICENSE|CHANGELOG|MAINTAINERS|INSTALL|UPGRADE|API|README).*\.txt$">
+    Order deny,allow
+    Deny from all
   </FilesMatch>
 
-  # Requires mod_expires to be enabled.
-  <IfModule mod_expires.c>
-    # Enable expirations.
-    ExpiresActive On
-
-    # Cache all files for 2 weeks after access (A).
-    ExpiresDefault A1209600
-
-    <FilesMatch \.php$>
-      # Do not allow PHP scripts to be cached unless they explicitly send cache
-      # headers themselves. Otherwise all scripts would have to overwrite the
-      # headers set by mod_expires if they want another caching behavior. This may
-      # fail if an error occurs early in the bootstrap process, and it may cause
-      # problems if a non-Drupal PHP file is installed in a subdirectory.
-      ExpiresActive Off
-    </FilesMatch>
-  </IfModule>
-
-  RewriteEngine On
-  RewriteBase /
-        <Files "cron.php">
+  <Files "cron.php">
     Order Deny,Allow
     Deny from all
     Allow from localhost
     Allow from 127.0.0.1
   </Files>
-  # Rewrite URLs of the form 'index.php?q=x'.
-  RewriteCond %{REQUEST_FILENAME} !-f
-  RewriteCond %{REQUEST_FILENAME} !-d
-  RewriteRule ^(.*)$ index.php?q=$1 [L,QSA]
 </Directory>
-<VirtualHost *:<?php print $port ?>>
+<VirtualHost *:<?php print $port; ?>>
   ServerName <?php print $hostname . PHP_EOL; ?>
   DocumentRoot <?php print $docroot . PHP_EOL; ?>
   LogLevel warn
   ServerSignature Off
 </VirtualHost>
-
